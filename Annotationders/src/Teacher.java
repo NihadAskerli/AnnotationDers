3d9833289ee0ba712b1e70528e7ec02e34fb085b
Nihad <esgerlinihad7@gmail.com>
2022-10-10 19:40:34 +0400

    new ders

---
+++ Annotationders/src/Teacher.java
@@ -0,0 +1,16 @@
+
+
+
+public class Teacher  {
+
+    private String name;
+    private String surname;
+
+    public void foo(int a) throws Exception {
+        if(a<9){
+            System.out.println("salam");
+        }
+    }
+
+
+}
